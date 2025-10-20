a965fd097dd346cae6547291b4399d60812a1777
Sam Siu <117384741+Muubii@users.noreply.github.com>
2025-10-20 19:59:06 +0200

    API-web-app

---
+++ src/pages/Home.js
@@ -0,0 +1,11 @@
+import '../assets/styles/App.css';
+
+export default function Home() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>Nothing beats a jet 2 holiday</p>
+      </header>
+    </div>
+  );
+}
